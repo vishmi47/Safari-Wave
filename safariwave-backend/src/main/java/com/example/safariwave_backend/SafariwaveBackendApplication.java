51c0b6d1776ee7204270438b384f0f8c0fd12505
vishmi47 <vishmisavindi@gmail.com>
2026-09-03 20:56:09 +0530

    feat: initial commit

---
+++ safariwave-backend/src/main/java/com/example/safariwave_backend/SafariwaveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.safariwave_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafariwaveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafariwaveBackendApplication.class, args);
+	}
+
+}
