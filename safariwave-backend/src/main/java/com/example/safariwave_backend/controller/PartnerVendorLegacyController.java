b29570bdcccfa1bbbaab9895f2dac40a479195ca
vishmi47 <vishmisavindi@gmail.com>
2026-09-15 13:42:04 +0530

    Delete safariwave-backend/src/main/java/com/example/safariwave_backend/controller/PartnerVendorLegacyController.java

--- safariwave-backend/src/main/java/com/example/safariwave_backend/controller/PartnerVendorLegacyController.java
+++
@@ -1,9 +0,0 @@
-package com.example.safariwave_backend.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
-public class PartnerVendorLegacyController {
-}
